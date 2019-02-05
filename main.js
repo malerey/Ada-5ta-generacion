d39ff8b0d5d392aac49abb90d6c9c166ce2859e4
Malena Rey <36973939+malerey@users.noreply.github.com>
2019-02-05 18:04:31 -0300

    Clase 5/2 - Ejercicios y estructuras de repeticion

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 
+// Ejercicio 69
+// Crear un documento con el nombre ej69.js
+// Mostrar en consola la suma parcial de todos los números que van desde 0 hasta 1000 utilizando la estructura while.
 
-//propiedades y metodos de strings 
 
 
-// propiedad LENGTH
-//saber cuántos caracteres 
-// tiene un string.
-// Devuelve un NUMERO 
-//con la cantidad de caracteres 
 
-// var lalala = "Hola";
-// var cantidadDeCaracteres = lalala.length;
-// console.log(cantidadDeCaracteres);
 
+// (0) + 1 = 1
+// (0 + 1) + 2 = 3
+// (0 + 1 + 2) + 3 = 6
+// (0 + 1 + 2 + 3) + 4 = 10
+// (0 + 1 + 2 + 3 + 4) + 5 = 15
 
 
 
+// 0 + 1 = 1
+// 1 + 2 = 3
+// 3 + 3 = 6
+// 6 + 4 = 10
+// 10 + 5 = 15
 
 
 
 
 
 
-// tambien puede utilizarse sin declarar 
-// // una nueva variable
-// var otroTexto = 'Aditasss!';
-// console.log(otroTexto.length);
+// inicio de la serie + incremento de 1 = total
 
 
 
@@ -37,287 +38,269 @@
 
 
 
-// var textoConEspacios = 
-// "    var numero = 2    !        &       )"
 
 
 
-// console.log(textoConEspacios.length);
+// var total = 0
+// var incremento = 1
 
-
-
-
-
-
-
-
-
-// Mostrar en consola el siguiente mensaje:
-// Mi nombre es *nombre* y tiene 
-// *cantidad de caracteres* letras
-
-
-// Ejemplo: mi nombre es Malena 
-// y tiene 6 letras 
-
-
-
-
-
-
-
-
-
-
-
-// var nombre = "Malena"
-// console.log("Mi nombre es ", 
-// nombre, " y tiene ", nombre.length, " letras")
-
-
-
-
-
-
-// metodos
-
-// los metodos reciben algo y 
-//devuelven otra cosa
-// ponemos entre parentesis lo 
-//que queremos 
-//que el metodo reciba
-
-// a eso lo llamamos "parametros"
-
-// sintaxis:
-// nombreDeLaVariable.metodo(parametros)
-
-// a veces no es necesario el nombre de la variable
-// a veces el metodo no recibe parametros
-
-
-
-
-
-
-
-// // // Concat
-// var saludo = 'Hola ';
-// var nombre = 'Ada';
-
-// // Concatenamos los strings 
-// // usando el método concat
-
-// console.log(saludo.concat(nombre));
-
-
-
-
-
-
-
-
-
-
-
-
-//Tambien se pueden encadenar
-// varios strings, 
-// // separandolos con coma
-// Ejercicio: lograr que la consola diga 
-// "Hola Marina, Sofia y Luciana"
-
-
-
-
-
-
-
-
-
-
-
-// var saludo = "Hola ";
-// var nombre = "Marina, ";
-// var otroNombre = "Sofia ";
-// var otroNombreMas = "y Luciana";
-// console.log(saludo.concat(nombre, otroNombre, otroNombreMas))
-
-
-
-// var saludo = "Hola";
-// var nombre = "Marina";
-// var otroNombre = "Sofia";
-// var otroNombreMas = "Luciana";
-// var espacio = " "
-// var coma = ","
-// var y = " y "
-// console.log(saludo.concat(espacio, nombre, coma, espacio, otroNombre, espacio, y, espacio, otroNombreMas))
-
-
-// console.log(saludo.concat(", ", nombre))
-
-
-
-
-
-
-
-
-
-
-// Mayúsculas y minúsculas
-// var saludo = 'HOLA';
-// var aditas = 'aditas';
-// var messenger = "hOlA aDiTaS"
-
-// var textoEnMinusculas = saludo.toLowerCase();
-// var textoEnMayusculas = aditas.toUpperCase();
-// var escribimosNormal = messenger.toLowerCase();
-
-// console.log("texto en minusculas", textoEnMinusculas)
-// console.log("texto en mayusculas", textoEnMayusculas)
-// console.log("texto en minusculas", escribimosNormal)
-
-
-
-
-
-//Tambien podemos hacerlo 
-// sin declarar la variable
-// console.log("HOLA ADITAS".toLowerCase())
-
-
-
-
-
-
-
-
-// Métodos de Number
-
-
-
-
-// parseInt
-// convierte un string a 
-// un numero ENTERO
-// devuelve un numero
-
-// var numero = "3"
-// console.log(parseInt(numero))
-
-// var numeroPi = "3.14"
-// console.log(parseInt(numeroPi))
-
-
-
-
-
-
-
-// si el string no es convertible 
-//a numero??
-
-// var otroNumero = "hola"
-// console.log(parseInt(otroNumero))
-
-
-
-
-
-
-
-
-
-
-
-
-//  parseFloat
-// Toma como parametro un string 
-// y devuelve un número decimal
-// el numero se escribe a la manera inglesa 
-// (punto en lugar de coma)
-
-// var piEnTexto = '3.14';
-// var pi = parseFloat(piEnTexto);
-// console.log(pi);
-
-
-
-
-
-// toString
-// Recibe un numero, retorna un string 
-// con el valor del número
-
-// var numero = 4;
-// var numeroEnString = numero.toString();
-// console.log(numeroEnString)
-
-
-
-
-
-
-
-
-
-// atencion! 
-// recordemos que el signo + 
-// significa cosas distintas
-// si estamos lidiando con numeros 
-// o con strings
-
-// si se trata de numeros, SUMA
-// si se trata de strings, CONCATENA
-
-// si se trata de ambos
-// CONVIERTE A STRING EL NUMERO
-//  Y CONCATENA 
-
-
-
-
-
-
-
-
-//ejercicio
-//dadas las siguientes variables
-
-// var nueve = 9;
-// var nueveEnTexto = "9";
-// var dos = 2;
-// var dosEnTexto = "2";
-
-// Imprimir en consola:
-// 11
-// 92
-// 911
-
-
-
-
-
-
-
-
-
-
-// IF
-
-// var contrasena = "123"
-// var nombreUsuario = "lala"
-
-// if (contrasena.length >= 10 && contrasena.length <= 40) {
-//   console.log("tu password es correcto")
+// while (incremento < 10) {
+//   console.log(total, incremento, total + incremento)
+//   total = total + incremento
+//   incremento++
 // }
-// else if (contrasena.length > 40) {
-//   console.log("tu password es muy largo")
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejercicio 70
+// Mostrar en consola la suma parcial de 
+// todos los números impares que van desde 
+// 0 hasta 1000 utilizando la estructura while.
+
+// var total = 0
+// var incremento = 1
+
+// while (incremento < 100) {
+//   console.log(total, incremento, total + incremento)
+//   total = total + incremento
+//   incremento = incremento + 2
+// }
+
+
+
+
+
+
+
+
+
+// Ejercicio 71
+// Mostrar en consola la tabla de multiplicar del 9 
+
+
+// var numero = 1
+// var incremento = 9
+
+// while (numero <= 10) {
+//   console.log(numero*incremento)
+//   numero++
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejercicio 72
+// Crear un documento con el nombre ej72.js
+// Mostrar en consola el siguiente
+// dibujo utilizando la estructura while
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
+// ************
+// *************
+
+
+
+// ---> primera intuicion:
+// var f = "*"
+// while ( f.length < 15) {
+//  f = f+f
+// }
+// ------- > 
+// *
+// **
+// ****
+// ********
+// ****************
+
+
+
+
+// var f = "*";
+// var f2 = "*";
+// while (f.length < 15) {
+//   console.log(f)
+//   f = f + f2
+// }
+
+
+
+
+
+// var f = "*";
+// var f2 = "*";
+// while (f.length < 15) {
+//   console.log(f)
+//  f = f.concat(f2)
+// }
+
+
+
+// var numero = 1;
+// var f = "*";
+// while (numero <= 13) {
+  //  console.log(f);
+//  f += "*";
+// //  // // // f = f + "*"
+//     numero++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejercicio 73
+// Crear un documento con el nombre ej73.js
+// Mostrar en consola el siguiente dibujo utilizando la estructura while
+// *************
+// ************
+// ***********
+// **********
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+
+// var contador = 5;
+// var acumulado = "";
+// var estrellita = "******";
+
+// while (contador>0) {
+//    acumulado = estrellita.slice(0,contador);
+//    console.log(acumulado);
+//    contador--;
+// }
+
+// var numero = 0;
+// var linea = "*************";
+
+// while (numero < linea.length) {
+//   var substring = linea.substring(numero);
+//   console.log(substring, substring.length)
+//   numero++;
 // } 
-// else {
-//   console.log("tu password no es valido")
+
+
+
+
+
+
+
+
+
+
+
+// var a = "";
+// var base = 5;
+// var resta = 5;
+
+// while (base > 0) {
+//   while (resta > 0) {
+//     a = a + "*"
+//     resta--
+//   }
+//   console.log(a)
+//   resta = base - 1
+//   a = ""
+//   base--
+// }
+
+
+
+
+
+
+
+
+// Ejercicio 74
+// *
+// ***
+// *****
+// *******
+// *********
+// ***********
+// *************
+
+// var numero = 1;
+// var items = "**";
+// var linea = "*";
+
+// while (numero < 7) {
+//   console.log(linea);
+//     linea += item;
+//     numero++;
+// }
+
+
+
+
+
+
+// Ejercicio 75
+// Fibonacci
+
+
+
+
+
+// 0 1 1 2 3 5 8 13 
+
+
+
+
+
+
+
+// var a = 0
+// var b = 1
+// var c = 1
+
+
+// while (a<100) {
+//   console.log(a)
+//   a = b
+//   b = c
+//   c = a+b
 // }
 
 
@@ -333,91 +316,64 @@
 
 
 
+// do/while
+// siempre se ejecuta al menos una vez y luego pregunta por la condición
+// Ejemplo:
+
+// do {
+//   // esto se ejecuta al menos una vez
+// } while (condicion)
+
+
+// var numero = 1000;
+
+// do {
+//   console.log('número: ', numero);
+//   numero++;
+// } while (numero < 10)
+
+
+
+
+// ejercicio 67
+// Mostrar en consola los números del 
+// 100 al 0 utilizando la estructura do/while
 
 
 
 
 
-// if
 
-// if (condition) {
+// for
+// Consiste en 3 partes delimitadas por un ;
+// Ejemplo:
 
+// for (iniciarVariable; condicion; alteración) {
+//   // se ejecuta siempre y cuando se cumpla la condición
 // }
 
-// en la condicion debe ir un BOOLEANO
-// -- caso contrario javascript fuerza 
-// la interpretacion (valores truthy y falsy que veremos despues)
+// Primero: declaramos la variable numero y le asignamos el valor 0
+// Segundo: escribimos la condición que queremos validar
+// Terceo: incrementamos el valor de la variable numero en 1
 
 
-
-
-
-
-
-
-
-//operador ternario 
-// var numero = 10;
-
-// numero == 10 
-// ? console.log("es diez") 
-// : console.log("no es diez");
-
-//es exactamente igual a : 
-
-// if (numero == 10) {
-//   console.log("es diez") 
-// } else {
-//   console.log("no es diez")
-// };
-
-
-
-
-
-
-
-
-
-
-//switch
-
-// var nombre = 'Marta';
-// var mensaje = null
-
-// switch (nombre) {
-//   case 'Marta':
-//     mensaje = 'es Marta';
-//     break;
-//   case 'Felipa':
-//     mensaje = 'es Felipa';
-//     break;
-//   case 'Xime':
-//     mensaje = 'es Xime';
-//     break;
-//   case 'Belu':
-//     mensaje = 'es Belu';
-//     break;
-//   default:
-//      mensaje = 'El nombre de la usuaria no es Marta, Felipa, Xime o Belu';
+// for (var numero = 0; numero <= 10; numero++) {
+//   console.log(numero);
 // }
-// console.log(mensaje)
 
 
 
+//ejercicio 96
+// Mostrar en consola los números 
+// de 10 en 10 desde el 0 hasta el 10000
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//break en estricturas de repeticion
+// for (var i = 0; i < 1000; i++){
+//   if (i < 10) {
+//     console.log(i);
+//   } else {
+//     break;
+//   }
+// }
