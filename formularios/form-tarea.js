0dea73df8f07cc30aeb9777ccc01b71568f3844a
MalenaRey <reymalena@gmail.com>
2019-04-12 20:27:08 -0300

    update

--- formularios/form-tarea.js
+++ formularios/form-tarea.js
@@ -16,6 +16,7 @@ formulario.onsubmit = function(e) {
   var nombre = nombre_form.value
   var edad = edad_form.value
 
+
   // Creamos una variable "estado" y le asignamos el valor
   // del checkbox que este seleccionado. 
   // Ejemplo, si "soltero" esta seleccionado, "estado" va a ser "soltero"
@@ -33,22 +34,31 @@ formulario.onsubmit = function(e) {
   // Sacamos el indice de la opcion seleccionada
   // Usando ese indice, buscamos la opcion elegida
   // Asignamos la opcion elegida a la variable "nacionalidad"
+
+
   var i = nacionalidad_form.selectedIndex
+
   var nacionalidad = nacionalidad_form.options[i].value
 
   // console.log para chequear que nuestros datos esten bien 
 
-  // console.log(nombre, edad)
-  // console.log(estado)
-  // console.log(nacionalidad)
+  console.log(nombre, edad)
+  console.log(estado)
+  console.log(nacionalidad)
 
 
-  // Ahora validamos, marcando como error en el form: 
+//Ahora validamos, marcando como error en el form: 
 
   // Nombre no tiene que estar vacio
   if (nombre.length === 0) {
     nombre_form.classList.add("error")
   }
+
+// version alternativa (mas linda)
+  // if (!nombre) {
+  //   nombre_form.classList.add("error")
+  // }
+
 
   // edad debe ser mayor a 18 y menor a 120
   if (edad < 18 || edad > 120) {
@@ -56,23 +66,144 @@ formulario.onsubmit = function(e) {
   }
 
   if (estado === 'vacio') {
+
     soltero_form.nextElementSibling.classList.add("error") 
     casado_form.nextElementSibling.classList.add("error") 
   }
 
-// nacionalidades se "autovalidan", 
-// porque ya sabemos que estan seleccionadas. 
+// // nacionalidades se "autovalidan", 
+// // porque ya sabemos que estan seleccionadas. 
 
 
-if (nombre.length > 0 && (edad > 18 && edad < 120) && estado !== 'vacio') {
+if (nombre.length > 0 
+  && (edad > 18 
+    && edad < 120) 
+  && estado !== 'vacio') {
   agregarInvitado(nombre, edad, estado, nacionalidad)
+  }
 }
-}
+
+
 
 function agregarInvitado(nombre, edad, estado, nacionalidad) {
-console.log(nombre, edad, estado, nacionalidad)
-var parrafo = document.createElement("p");
-parrafo.textContent = nombre + " " + edad + " " + estado + " " + nacionalidad
-document.body.appendChild(parrafo);
 
+  if (estado === "taken") {
+    estado = "No disponible"
+  }
+  else if (estado === "single") {
+    estado = "Disponible"
+  }
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina"
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana"
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana"
+  }
+
+var lista = document.getElementById("lista-de-invitados")
+
+var elementoLista = document.createElement("li")
+
+elementoLista.classList.add("elemento-lista")
+
+elementoLista.textContent = "Nombre: " + nombre + ". Edad: " + edad + ". Preferencia: " + estado + ". Nacionalidad: " + nacionalidad
+// elementoLista.innerHTML = "Nombre: " + nombre + ". Edad: " + edad + ". Preferencia: " + estado + ". Nacionalidad: " + nacionalidad
+
+lista.appendChild(elementoLista)
+
+
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+elementoLista.appendChild(corteLinea)
+elementoLista.appendChild(botonBorrar);
+
+
+
+
+
+ botonBorrar.onclick = function() {
+// this.parentNode.style.display = 'none';
+botonBorrar.parentNode.remove()
+  }
 }
+
+
+// function agregarInvitado(nombre, edad, estado, nacionalidad) {
+
+//   if (estado === "taken") {
+//     estado = "No disponible"
+//   }
+//   else if (estado === "single") {
+//     estado = "Disponible"
+//   }
+
+//   if (nacionalidad === "ar") {
+//     nacionalidad = "Argentina"
+//   }
+//   else if (nacionalidad === "vnzl") {
+//     nacionalidad = "Venezolana"
+//   }
+//   else if (nacionalidad === "per") {
+//     nacionalidad = "Peruana"
+//   }
+
+// var lista = document.getElementById("lista-de-invitados")
+
+// var elementoLista = document.createElement("div")
+// elementoLista.classList.add("elemento-lista")
+// lista.appendChild(elementoLista)
+
+// // var spanNombre = document.createElement("span")
+// // var inputNombre = document.createElement("input")
+// // var espacio = document.createElement("br")
+// // spanNombre.textContent = "Nombre: "
+// // inputNombre.value = nombre 
+// // elementoLista.appendChild(spanNombre)
+// // elementoLista.appendChild(inputNombre)
+// // elementoLista.appendChild(espacio)
+
+
+
+
+
+// function crearElemento(descripcion, valor) {
+// var spanNombre = document.createElement("span")
+// var inputNombre = document.createElement("input")
+// var espacio = document.createElement("br")
+// spanNombre.textContent = descripcion + ": "
+// inputNombre.value = valor 
+// elementoLista.appendChild(spanNombre)
+// elementoLista.appendChild(inputNombre)
+// elementoLista.appendChild(espacio)
+// }
+
+// crearElemento("Nombre", nombre)
+// crearElemento("Edad", edad)
+// crearElemento("Nacionalidad", nacionalidad)
+// crearElemento("Estado", estado)
+
+// var botonBorrar = document.createElement("button")
+// botonBorrar.textContent = "Eliminar invitado"
+// botonBorrar.id = "boton-borrar"
+// var corteLinea = document.createElement("br")
+// elementoLista.appendChild(corteLinea)
+// elementoLista.appendChild(botonBorrar);
+
+
+
+
+//  botonBorrar.onclick = function() {
+// // this.parentNode.style.display = 'none';
+// botonBorrar.parentNode.remove()
+//   }
+// }
+
+
+
